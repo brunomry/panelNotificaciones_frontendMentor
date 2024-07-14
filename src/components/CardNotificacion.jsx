5fbbee2e6736e1ddd658596dc89e32dd1788bfa3
brunomry <brunomadozzo@gmail.com>
2024-07-14 12:17:38 -0300

    agregue estructura de proyecto panel de notificaciones de frontend mentor e instale dependencia de tailwind

---
+++ src/components/CardNotificacion.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardNotificacion = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CardNotificacion;
---
+++ src/components/Notificaciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notificaciones = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Notificaciones;
